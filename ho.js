e2796d2b99713f8c5cfd0f1d3c3d5bbe04fd4f3c
Daniel Ors <do7811@gmail.com>
2019-01-15 16:55:12 -0500

    working on reducer

--- ho.js
+++ ho.js
@@ -74,4 +74,28 @@ posMovements.forEach( ( element ) => console.log(`Our turtle took a total of ${e
 
 // reduce function
 
-  
+const input = 'noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest';
+
+// const inputArray = input.reduce(' ');
+
+// // inputArray.forEach(inputArray[i] = 3)
+
+// let accumulator;
+
+// switch(inputArray[i].length) {
+// case 3 : 
+//   return ' ';
+// default : 
+//   inputArray[i][i.length - 1].toUpperCase;
+// }
+
+const decoder = function(input) { 
+  return input.split(' ').reduce((acc, cur) => {
+    if ( cur = 3 ) {
+      acc.push(' ');
+    }
+    else {
+      cur[cur.length - 1].toUppercase();
+  }
+    return acc;
+  };
